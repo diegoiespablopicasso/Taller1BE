0067706ad4b53f34a3858743f1059d557e261d81
diegoiespablopicasso <dbenaventevelasco@gmail.com>
2021-01-21 10:26:51 +0100

    Agregar archivos de proyecto.

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Taller.BL.Contracts;
+using Taller.Core.DTO;
+
+namespace Taller.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LoginController
+    {
+        public IUsuarioBL _usuarioBL { get; set; }
+
+        public LoginController(IUsuarioBL pollo)
+        {
+            _usuarioBL = pollo;
+        }
+
+        [HttpPost]
+        public bool Login(UsuarioDTO usuarioDTO)
+        {
+            return _usuarioBL.Login(usuarioDTO);
+        }
+    }
+}
